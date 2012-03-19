e24033166ee83c1507d4da917b8f2d09fe372bc6
Trygve Vea <trygve.vea@gmail.com>
2012-03-19 22:01:31 +0100

    Added a Base1024 Number format

--- src/js/Rickshaw.Fixtures.Number.js
+++ src/js/Rickshaw.Fixtures.Number.js
@@ -9,3 +9,14 @@ Rickshaw.Fixtures.Number.formatKMBT = function(y) {
 	else if (y == 0)          { return '' }
 	else                      { return y }
 };
+
+Rickshaw.Fixtures.Number.formatBase1024KMGTP = function(y) {
+    if (y >= 1125899906842624)  { return y / 1125899906842624 + "P" }
+    else if (y >= 1099511627776){ return y / 1099511627776 + "T" }
+    else if (y >= 1073741824)   { return y / 1073741824 + "G" }
+    else if (y >= 1048576)      { return y / 1048576 + "M" }
+    else if (y >= 1024)         { return y / 1024 + "K" }
+    else if (y < 1 && y > 0)    { return y.toFixed(2) }
+    else if (y == 0)            { return '' }
+    else                        { return y }
+};
