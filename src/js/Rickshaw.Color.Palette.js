831830c017ed2091ca698ba2a7bb091ad711e6b2
Travis Beck <travis@shutterstock.com>
2012-06-06 08:55:01 -0400

    add color interpolation to get more colors than your colorscheme specifies, but still in the same general range

--- src/js/Rickshaw.Color.Palette.js
+++ src/js/Rickshaw.Color.Palette.js
@@ -9,8 +9,25 @@ Rickshaw.Color.Palette = function(args) {
 
 	this.scheme = color.schemes[args.scheme] || args.scheme || color.schemes.colorwheel;
 	this.runningIndex = 0;
+	this.generatorIndex = 0;
+  this.rotateCount = this.scheme.length;
 
 	this.color = function(key) {
-		return this.scheme[key] || this.scheme[this.runningIndex++] || '#808080';
+		return this.scheme[key] || this.scheme[this.runningIndex++] || this.interpolateColor() || '#808080';
 	};
+
+  this.interpolateColor = function() {
+    var color;
+    if (this.generatorIndex == this.rotateCount * 2 - 1) {
+      color = d3.interpolateHsl(this.scheme[this.generatorIndex], this.scheme[0])(0.5);
+      this.generatorIndex = 0;
+      this.rotateCount *= 2;
+    } else {
+      color = d3.interpolateHsl(this.scheme[this.generatorIndex], this.scheme[this.generatorIndex + 1])(0.5);
+      this.generatorIndex++;
+    }
+    this.scheme.push(color);
+    return color;
+  };
+
 };
