a0149ac085f6a58ba05207bafe313b548529e894
Travis Beck <travis@shutterstock.com>
2012-06-07 20:16:47 -0400

    fix the colorscheme arg to interpolatedStopCount per david's suggestion

--- src/js/Rickshaw.Color.Palette.js
+++ src/js/Rickshaw.Color.Palette.js
@@ -11,14 +11,14 @@ Rickshaw.Color.Palette = function(args) {
 	this.runningIndex = 0;
 	this.generatorIndex = 0;
 
-  if (args.preInterpolateCount) {
+  if (args.interpolatedStopCount) {
     var schemeCount = this.scheme.length - 1;
     var i, j, scheme = [];
     for (i = 0; i < schemeCount; i++) {
       scheme.push(this.scheme[i]);
       var generator = d3.interpolateHsl(this.scheme[i], this.scheme[i + 1]);
-      for (j = 1; j < args.preInterpolateCount; j++) {
-        scheme.push(generator((1 / args.preInterpolateCount) * j));
+      for (j = 1; j < args.interpolatedStopCount; j++) {
+        scheme.push(generator((1 / args.interpolatedStopCount) * j));
       }
     }
     scheme.push(this.scheme[this.scheme.length - 1]);
