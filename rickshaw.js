f69eaab2e630ad7f06f9e8c813133d41e3f4349b
JT <jt@gosquared.com>
2012-10-01 12:37:55 +0100

    Fix global tickValue as well

--- rickshaw.js
+++ rickshaw.js
@@ -1191,6 +1191,7 @@ Rickshaw.Graph.Axis.Time = function(args) {
 		var runningTick = domain[0];
 
 		var offsets = [];
+		var tickValue;
 
 		for (var i = 0; i < count; i++) {
 
@@ -1214,9 +1215,9 @@ Rickshaw.Graph.Axis.Time = function(args) {
 		var offsets = this.tickOffsets();
 
 		offsets.forEach( function(o) {
-			
+
 			if (self.graph.x(o.value) > self.graph.x.range()[1]) return;
-	
+
 			var element = document.createElement('div');
 			element.style.left = self.graph.x(o.value) + 'px';
 			element.classList.add('x_tick');
