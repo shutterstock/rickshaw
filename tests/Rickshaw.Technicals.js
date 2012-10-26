6e49627ec6c3b17ddec8282e70a3e61605a81f12
Kevin Mulvey <king.feruke@gmail.com>
2012-10-26 16:58:10 -0400

    start sma tests

--- tests/Rickshaw.Technicals.js
+++ tests/Rickshaw.Technicals.js
@@ -1,20 +1,7 @@
 var Rickshaw = require('../rickshaw'), assert = require('assert');
 
-////////////   MOMENTUM
-function momentum(sample_data, period, test){
-  var tech = new Rickshaw.Technicals.Momentum();
-	    
-  var data = tech.calc({
-    datum: sample_data,
-    period: period
-	});
 
-  for(var i in data.momentum){
-    if(i < period['p']) continue;
-	  test.equal(data.momentum[i].y, sample_data[i].y - sample_data[i-period['p']].y, 'constant momentum');
-  }
-  test.done();
-}
+////////////   UTILS
 function seriesData(arg) {
   var data = {
 	  constant : [ {x: 0, y: 0, y0: 0}, {x: 1, y: 1, y0: 0}, { x: 2, y: 2, y0: 0} , { x: 3, y: 3, y0: 0}, { x: 4, y: 4, y0: 0}, { x: 5, y: 5, y0: 0}, { x: 6, y: 6,y0: 0}, { x: 7, y: 7, y0: 0}, { x: 8, y: 8, y0: 0}],
@@ -24,11 +11,65 @@ function seriesData(arg) {
   return data[arg];
 };
 
+
+////////////   MOMENTUM
+function momentum(sample_data, period, test, msg){
+  var tech = new Rickshaw.Technicals.Momentum();
+	    
+  var data = tech.calc({
+    datum: sample_data,
+    period: period
+	});
+
+  for(var i in data.momentum){
+    if(i < period['p']) continue;
+	  test.equal(data.momentum[i].y, sample_data[i].y - sample_data[i-period['p']].y, msg);
+  }
+  test.done();
+}
+
 exports.constantMomentum = function(test) {
   var period = new Array();
   period['p'] = 1;
   var sample_data = seriesData('constant');
-  momentum(sample_data, period, test);
+  momentum(sample_data, period, test, 'constant momentum');
+};
+
+exports.fibMomentum = function(test) {
+  var period = new Array();
+  period['p'] = 2;
+  var sample_data = seriesData('fib');
+  momentum(sample_data, period, test, 'fib momentum');
+};
+exports.primeMomentum = function(test) {
+  var period = new Array();
+  period['p'] = 3;
+  var sample_data = seriesData('prime');
+  momentum(sample_data, period, test, 'prime momentum');
+};
+
+
+////////////   SMA
+function sma(sample_data, period, test, msg){
+  var tech = new Rickshaw.Technicals.SMA();
+	    
+  var data = tech.calc({
+    datum: sample_data,
+    period: period
+	});
+
+  for(var i in data.momentum){
+    if(i < period['p']) continue;
+	  test.equal(data.momentum[i].y, sample_data[i].y - sample_data[i-period['p']].y, msg);
+  }
+  test.done();
+}
+
+exports.constantSMA = function(test) {
+  var period = new Array();
+  period['p'] = 1;
+  var sample_data = seriesData('constant');
+  momentum(sample_data, period, test, 'constant sma');
 };
 
 exports.fibMomentum = function(test) {
