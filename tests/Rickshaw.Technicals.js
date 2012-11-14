b49c60a09252716dbc159c7796971c710f8229ce
Kevin <king.feruke@gmail.com>
2012-11-14 11:23:06 -0500

    add tests and correct broken copy/paste

--- tests/Rickshaw.Technicals.js
+++ tests/Rickshaw.Technicals.js
@@ -2,20 +2,31 @@ var Rickshaw = require('../rickshaw'), assert = require('assert');
 
 
 ////////////   UTILS
-function seriesData(arg) {
-  var data = {
+var sample_data = {
+  data : {
 	  constant : [ {x: 0, y: 0, y0: 0}, {x: 1, y: 1, y0: 0}, { x: 2, y: 2, y0: 0} , { x: 3, y: 3, y0: 0}, { x: 4, y: 4, y0: 0}, { x: 5, y: 5, y0: 0}, { x: 6, y: 6,y0: 0}, { x: 7, y: 7, y0: 0}, { x: 8, y: 8, y0: 0}],
     fib : [ {x: 0, y: 0, y0: 0}, {x: 1, y: 1, y0: 0}, { x: 1, y: 1, y0: 0} , { x: 2, y: 2, y0: 0}, { x: 3, y: 3, y0: 0}, { x: 5, y: 5, y0: 0}, { x: 8, y: 8,y0: 0}, { x: 13, y: 13, y0: 0}, { x: 21, y: 21, y0: 0}],
     prime : [ {x: 2, y: 2, y0: 0}, {x: 3, y: 3, y0: 0}, { x: 5, y: 5, y0: 0} , { x: 7, y: 7, y0: 0}, { x: 11, y: 11, y0: 0}, { x: 13, y: 13, y0: 0}, { x: 17, y: 17,y0: 0}, { x: 19, y: 19, y0: 0}, { x: 23, y: 23, y0: 0}]
-  };
-  return data[arg];
+  },
+  results : {
+    sma : {
+      constant : [ {x: 0, y: null, y0: 0}, {x: 1, y: null, y0: 0}, { x: 2, y: 1, y0: 0} , { x: 3, y: 2, y0: 0}, { x: 4, y: 3, y0: 0}, { x: 5, y: 4, y0: 0}, { x: 6, y: 5,y0: 0}, { x: 7, y: 6, y0: 0}, { x: 8, y: 7, y0: 0}],
+      fib : [ {x: 0, y: null, y0: 0}, {x: 1, y: null, y0: 0}, { x: 1, y: null, y0: 0} , { x: 2, y: null, y0: 0}, { x: 3, y: 1.4, y0: 0}, { x: 5, y: 2.4, y0: 0}, { x: 8, y: 3.8,y0: 0}, { x: 13, y: 6.2, y0: 0}, { x: 21, y: 10, y0: 0}],
+      prime : [ {x: 2, y: null, y0: 0}, {x: 3, y: null, y0: 0}, { x: 5, y: null, y0: 0} , { x: 7, y: null, y0: 0}, { x: 11, y: 5.6, y0: 0}, { x: 13, y: 7.8, y0: 0}, { x: 17, y: 10.6,y0: 0}, { x: 19, y: 13.4, y0: 0}, { x: 23, y: 16.6, y0: 0}]
+    },
+    stochastic : {
+      constant : [ {x: 0, y: null, y0: 0}, {x: 1, y: null, y0: 0}, { x: 2, y: 1, y0: 0} , { x: 3, y: 2, y0: 0}, { x: 4, y: 3, y0: 0}, { x: 5, y: 4, y0: 0}, { x: 6, y: 5,y0: 0}, { x: 7, y: 6, y0: 0}, { x: 8, y: 7, y0: 0}],
+      fib : [ {x: 0, y: null, y0: 0}, {x: 1, y: null, y0: 0}, { x: 1, y: null, y0: 0} , { x: 2, y: null, y0: 0}, { x: 3, y: 1.4, y0: 0}, { x: 5, y: 2.4, y0: 0}, { x: 8, y: 3.8,y0: 0}, { x: 13, y: 6.2, y0: 0}, { x: 21, y: 10, y0: 0}],
+      prime : [ {x: 2, y: null, y0: 0}, {x: 3, y: null, y0: 0}, { x: 5, y: null, y0: 0} , { x: 7, y: null, y0: 0}, { x: 11, y: 5.6, y0: 0}, { x: 13, y: 7.8, y0: 0}, { x: 17, y: 10.6,y0: 0}, { x: 19, y: 13.4, y0: 0}, { x: 23, y: 16.6, y0: 0}]
+    }
+  }
+  //return data[arg];
 };
 
 
 ////////////   MOMENTUM
-function momentum(sample_data, period, test, msg){
+function momentum(data_set, period, test, msg){
   var tech = new Rickshaw.Technicals.Momentum();
-	    
   var data = tech.calc({
     datum: sample_data,
     period: period
@@ -23,7 +34,7 @@ function momentum(sample_data, period, test, msg){
 
   for(var i in data.momentum){
     if(i < period['p']) continue;
-	  test.equal(data.momentum[i].y, sample_data[i].y - sample_data[i-period['p']].y, msg);
+	  test.equal(data.momentum[i].y, sample_data.data[data_set][i].y - sample_data.data[data_set][i-period['p']].y, msg);
   }
   test.done();
 }
@@ -31,56 +42,83 @@ function momentum(sample_data, period, test, msg){
 exports.constantMomentum = function(test) {
   var period = new Array();
   period['p'] = 1;
-  var sample_data = seriesData('constant');
-  momentum(sample_data, period, test, 'constant momentum');
+  momentum('constant', period, test, 'constant momentum');
 };
-
 exports.fibMomentum = function(test) {
   var period = new Array();
   period['p'] = 2;
-  var sample_data = seriesData('fib');
-  momentum(sample_data, period, test, 'fib momentum');
+  momentum('fib', period, test, 'fib momentum');
 };
 exports.primeMomentum = function(test) {
   var period = new Array();
   period['p'] = 3;
-  var sample_data = seriesData('prime');
-  momentum(sample_data, period, test, 'prime momentum');
+  momentum('prime', period, test, 'prime momentum');
 };
 
 
-////////////   SMA
-function sma(sample_data, period, test, msg){
+////////////   sma
+function sma(data_set, period, test, msg){
   var tech = new Rickshaw.Technicals.SMA();
-	    
+	var results = sample_data.results.sma[data_set];
   var data = tech.calc({
-    datum: sample_data,
+    datum: sample_data.data[data_set],
     period: period
 	});
-
-  for(var i in data.momentum){
-    if(i < period['p']) continue;
-	  test.equal(data.momentum[i].y, sample_data[i].y - sample_data[i-period['p']].y, msg);
+  for(var i in data.sma){
+	  test.equal(data.sma[i].y, results[i].y, msg);
   }
   test.done();
 }
 
-exports.constantSMA = function(test) {
-  var period = new Array();
-  period['p'] = 1;
-  var sample_data = seriesData('constant');
-  momentum(sample_data, period, test, 'constant sma');
-};
-
-exports.fibMomentum = function(test) {
-  var period = new Array();
-  period['p'] = 2;
-  var sample_data = seriesData('fib');
-  momentum(sample_data, period, test);
-};
-exports.primeMomentum = function(test) {
+exports.constantsma = function(test) {
   var period = new Array();
   period['p'] = 3;
-  var sample_data = seriesData('prime');
-  momentum(sample_data, period, test);
+  sma('constant', period, test, 'constant sma');
 };
+
+exports.fibsma = function(test) {
+  var period = new Array();
+  period['p'] = 5;
+  sma('fib', period, test, 'fib sma');
+};
+
+exports.primesma = function(test) {
+  var period = new Array();
+  period['p'] = 5;
+  sma('prime', period, test, 'prime sma');
+};
+
+
+
+////////////   stochastic
+function stochastic(data_set, period, test, msg){
+  var tech = new Rickshaw.Technicals.FStochastic();
+	var results = sample_data.results.stochastic[data_set];
+  var data = tech.calc({
+    datum: sample_data.data[data_set],
+    period: period
+	});
+  for(var i in data.stochastic){
+	  test.equal(data.stochastic[i].y, results[i].y, msg);
+  }
+  test.done();
+}
+
+exports.constantstochastic = function(test) {
+  var period = new Array();
+  period['p'] = 3;
+  sma('constant', period, test, 'constant stochastic');
+};
+
+exports.fibstochastic = function(test) {
+  var period = new Array();
+  period['p'] = 5;
+  sma('fib', period, test, 'fib stochastic');
+};
+
+exports.primestochastic = function(test) {
+  var period = new Array();
+  period['p'] = 5;
+  sma('prime', period, test, 'prime stochastic');
+};
+
