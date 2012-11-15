1d36a538de78fca8084599748ed0179cdc7a0c20
Kevin Mulvey <king.feruke@gmail.com>
2012-11-15 17:45:33 -0500

    update file order in grunt

--- grunt.js
+++ grunt.js
@@ -60,8 +60,9 @@ module.exports = function(grunt) {
         },
         concat: {
             dist: {
-                src: ['<banner:meta.banner>', 'src/js/*.js'],
-                dest: 'dist/js/<%= pkg.name %>-<%= pkg.version %>.js'
+//                src: ['<banner:meta.banner>', 'src/js/*.js'],
+								src: ['src/js/Rickshaw.js', 'src/js/Rickshaw.Class.js', 'src/js/Rickshaw.Compat.ClassList.js', 'src/js/Rickshaw.Graph.js', 'src/js/Rickshaw.Fixtures.Color.js', 'src/js/Rickshaw.Fixtures.RandomData.js', 'src/js/Rickshaw.Fixtures.Time.js', 'src/js/Rickshaw.Fixtures.Number.js', 'src/js/Rickshaw.Color.Palette.js', 'src/js/Rickshaw.Graph.Ajax.js', 'src/js/Rickshaw.Graph.Annotate.js', 'src/js/Rickshaw.Graph.Axis.Time.js', 'src/js/Rickshaw.Graph.Axis.Y.js', 'src/js/Rickshaw.Graph.Behavior.Series.Highlight.js', 'src/js/Rickshaw.Graph.Behavior.Series.Order.js', 'src/js/Rickshaw.Graph.Behavior.Series.Toggle.js', 'src/js/Rickshaw.Graph.HoverDetail.js', 'src/js/Rickshaw.Graph.JSONP.js', 'src/js/Rickshaw.Graph.Legend.js', 'src/js/Rickshaw.Graph.RangeSlider.js', 'src/js/Rickshaw.Graph.Renderer.js', 'src/js/Rickshaw.Graph.Renderer.Line.js', 'src/js/Rickshaw.Graph.Renderer.Stack.js', 'src/js/Rickshaw.Graph.Renderer.Bar.js', 'src/js/Rickshaw.Graph.Renderer.Area.js', 'src/js/Rickshaw.Graph.Renderer.ScatterPlot.js', 'src/js/Rickshaw.Graph.Smoother.js', 'src/js/Rickshaw.Graph.Unstacker.js', 'src/js/Rickshaw.Series.js', 'src/js/Rickshaw.Series.FixedDuration.js', 'src/js/Rickshaw.Technicals.js'],
+								dest: 'dist/js/<%= pkg.name %>-<%= pkg.version %>.js'
             }
         },
         min: {
