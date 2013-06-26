a583015ce2278f943c9aa781a55f040416b4ee91
Greg Bacchus <greg+git@bacchus.co.nz>
2013-06-27 08:30:53 +1200

    Handle when there are no points in a dataset

--- src/js/Rickshaw.Graph.HoverDetail.js
+++ src/js/Rickshaw.Graph.HoverDetail.js
@@ -56,6 +56,9 @@ Rickshaw.Graph.HoverDetail = Rickshaw.Class.create({
 
 			var data = this.graph.stackedData[j++];
 
+			if (!data.length)
+				return;
+
 			var domainX = graph.x.invert(eventX);
 
 			var domainIndexScale = d3.scale.linear()
