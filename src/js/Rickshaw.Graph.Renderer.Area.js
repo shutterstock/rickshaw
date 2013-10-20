016f9fac5abd5fede9f08eb4d73a0cb13ac1a51f
David Chester <dchester@shutterstock.com>
2013-10-20 01:47:21 -0400

    tweak area renderer to play nicely with multi renderer; fixes #335; closes #336

--- src/js/Rickshaw.Graph.Renderer.Area.js
+++ src/js/Rickshaw.Graph.Renderer.Area.js
@@ -40,17 +40,25 @@ Rickshaw.Graph.Renderer.Area = Rickshaw.Class.create( Rickshaw.Graph.Renderer, {
 		return factory;
 	},
 
-	render: function() {
+	render: function(args) {
+
+		args = args || {};
 
 		var graph = this.graph;
+		var series = args.series || graph.series;
 
-		graph.vis.selectAll('*').remove();
+		var vis = args.vis || graph.vis;
+		vis.selectAll('*').remove();
 
 		// insert or stacked areas so strokes lay on top of areas
 		var method = this.unstack ? 'append' : 'insert';
 
-		var nodes = graph.vis.selectAll("path")
-			.data(this.graph.stackedData)
+		var data = series
+			.filter(function(s) { return !s.disabled })
+			.map(function(s) { return s.stack });
+
+		var nodes = vis.selectAll("path")
+			.data(data)
 			.enter()[method]("svg:g", 'g');
 
 		nodes.append("svg:path")
@@ -64,7 +72,7 @@ Rickshaw.Graph.Renderer.Area = Rickshaw.Class.create( Rickshaw.Graph.Renderer, {
 		}
 
 		var i = 0;
-		graph.series.forEach( function(series) {
+		series.forEach( function(series) {
 			if (series.disabled) return;
 			series.path = nodes[0][i++];
 			this._styleSeries(series);
