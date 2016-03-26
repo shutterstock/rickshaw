ccda0a3982eba6978e53ed26d64e4659f5e8c900
cesine <cesine@yahoo.com>
2016-03-25 22:27:59 -0400

    upgrade dependancies

--- tests/Rickshaw.Graph.Legend.js
+++ tests/Rickshaw.Graph.Legend.js
@@ -1,10 +1,11 @@
+var d3 = require("d3");
 var Rickshaw;
 
 exports.setUp = function(callback) {
 
 	Rickshaw = require('../rickshaw');
 
-	global.document = d3.select('html')[0][0].parentNode;
+	global.document = require("jsdom").jsdom("<html><head></head><body></body></html>");
 	global.window = document.defaultView;
 
 	new Rickshaw.Compat.ClassList();
