8ef5f54c8d8322edc9cce82a1259dcbabbb47102
Serhiy Zahoriya <serhiy.int@gmail.com>
2016-12-26 03:21:27 +0200

    Correct xMin and xMax with assorted data

--- src/js/Rickshaw.Graph.Renderer.js
+++ src/js/Rickshaw.Graph.Renderer.js
@@ -51,12 +51,16 @@ Rickshaw.Graph.Renderer = Rickshaw.Class.create( {
 
 				if (y < yMin) yMin = y;
 				if (y > yMax) yMax = y;
+
+				if (d.x == null) return;
+
+				var x = d.x;
+
+				if (x < xMin) xMin = x;
+				if (x > xMax) xMax = x;
 			} );
 
 			if (!series.length) return;
-
-			if (series[0].x < xMin) xMin = series[0].x;
-			if (series[series.length - 1].x > xMax) xMax = series[series.length - 1].x;
 		} );
 
 		xMin -= (xMax - xMin) * this.padding.left;
