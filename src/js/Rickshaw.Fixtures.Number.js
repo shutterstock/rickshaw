b7366f812494372f93c292977b49576f2852a30c
Mokeponi <mokeponi@gmail.com>
2017-04-20 15:34:20 +0300

    Fixes issue with negative values between 0 and -1
    Probably due to a typo, numbers between 0 and -1 were not formatted as expected.

--- src/js/Rickshaw.Fixtures.Number.js
+++ src/js/Rickshaw.Fixtures.Number.js
@@ -6,7 +6,7 @@ Rickshaw.Fixtures.Number.formatKMBT = function(y) {
 	else if (abs_y >= 1000000000) { return y / 1000000000 + "B" }
 	else if (abs_y >= 1000000)    { return y / 1000000 + "M" }
 	else if (abs_y >= 1000)       { return y / 1000 + "K" }
-	else if (abs_y < 1 && y > 0)  { return y.toFixed(2) }
+	else if (abs_y < 1 && abs_y > 0)  { return y.toFixed(2) }
 	else if (abs_y === 0)         { return '' }
 	else                      { return y }
 };
@@ -18,7 +18,7 @@ Rickshaw.Fixtures.Number.formatBase1024KMGTP = function(y) {
     else if (abs_y >= 1073741824)   { return y / 1073741824 + "G" }
     else if (abs_y >= 1048576)      { return y / 1048576 + "M" }
     else if (abs_y >= 1024)         { return y / 1024 + "K" }
-    else if (abs_y < 1 && y > 0)    { return y.toFixed(2) }
+    else if (abs_y < 1 && abs_y > 0)    { return y.toFixed(2) }
     else if (abs_y === 0)           { return '' }
     else                        { return y }
 };
