d42f5b1a17a03c27181d8e7dd863dc97eacb7e5f
cesine <cesine@yahoo.com>
2017-07-25 21:57:44 -0700

    added test for onConfigure for #568 #579 #306 #324 #320
    added resize in the series example to manually demo onConfigure

--- src/js/Rickshaw.Graph.RangeSlider.js
+++ src/js/Rickshaw.Graph.RangeSlider.js
@@ -25,16 +25,11 @@ Rickshaw.Graph.RangeSlider = Rickshaw.Class.create({
 				self.update();
 			}.bind(self));
 
-			(function() {
-				// When the loop exits, the value of i would be an out-of-bound index for the
-				// graphs array, which would cause problems when it's used in the onConfigure
-				// callback below. Since there are no block scopes in JS. Hence, a locally scoped
-				// copy.
-				var idx = i;
-				graphs[i].onConfigure(function() {
-					$(element)[0].style.width = graphs[idx].width + 'px';
+			(function(idx){
+				graphs[idx].onConfigure(function() {
+					$(this.element)[0].style.width = graphs[idx].width + 'px';
 				}.bind(self));
-			})();
+			})(i);
 		}
 
 	},
@@ -88,7 +83,7 @@ Rickshaw.Graph.RangeSlider = Rickshaw.Class.create({
 		} );
 
 		graphs[0].onConfigure(function() {
-			$(element)[0].style.width = graphs[0].width + 'px';
+			$(this.element)[0].style.width = graphs[0].width + 'px';
 		}.bind(this));
 
 	},
