c3a3175f9be1d8b25d2e351313a3509fe85c94ee
Your Name <you@example.com>
2018-10-07 21:22:59 +0200

    correct formatBase1024KMGTP and support negatives values
    https://github.com/shutterstock/rickshaw/issues/601

--- src/js/Rickshaw.Fixtures.Number.js
+++ src/js/Rickshaw.Fixtures.Number.js
@@ -1,24 +1,32 @@
 Rickshaw.namespace('Rickshaw.Fixtures.Number');
 
 Rickshaw.Fixtures.Number.formatKMBT = function(y) {
-	var abs_y = Math.abs(y);
-	if (abs_y >= 1000000000000)   { return y / 1000000000000 + "T" }
-	else if (abs_y >= 1000000000) { return y / 1000000000 + "B" }
-	else if (abs_y >= 1000000)    { return y / 1000000 + "M" }
-	else if (abs_y >= 1000)       { return y / 1000 + "K" }
-	else if (abs_y < 1 && abs_y > 0)  { return y.toFixed(2) }
-	else if (abs_y === 0)         { return '' }
-	else                      { return y }
+    var sign='';
+    if (y<0) {
+        sign='-';
+    }
+    var abs_y = Math.abs(y);
+    if (abs_y >= 1000000000000)      { return sign+(y / 1000000000000).toFixed(2) + "T" }
+    else if (abs_y >= 1000000000)    { return sign+(y / 1000000000).toFixed(2) + "B" }
+    else if (abs_y >= 1000000)       { return sign+(y / 1000000).toFixed(2) + "M" }
+    else if (abs_y >= 1000)          { return sign+(y / 1000).toFixed(2) + "K" }
+    else if (abs_y < 1 && abs_y > 0) { return sign+y.toFixed(2) }
+    else if (abs_y === 0)            { return sign+'0' }
+    else                             { return sign+y }
 };
 
 Rickshaw.Fixtures.Number.formatBase1024KMGTP = function(y) {
+    var sign='';
+    if (y<0) {
+            sign='-';
+    }
     var abs_y = Math.abs(y);
-    if (abs_y >= 1125899906842624)  { return y / 1125899906842624 + "P" }
-    else if (abs_y >= 1099511627776){ return y / 1099511627776 + "T" }
-    else if (abs_y >= 1073741824)   { return y / 1073741824 + "G" }
-    else if (abs_y >= 1048576)      { return y / 1048576 + "M" }
-    else if (abs_y >= 1024)         { return y / 1024 + "K" }
-    else if (abs_y < 1 && abs_y > 0)    { return y.toFixed(2) }
-    else if (abs_y === 0)           { return '' }
-    else                        { return y }
+    if (abs_y >= 1125899906842624)   { return sign+(y / 1125899906842624).toFixed(2) + "P" }
+    else if (abs_y >= 1099511627776) { return sign+(y / 1099511627776).toFixed(2) + "T" }
+    else if (abs_y >= 1073741824)    { return sign+(y / 1073741824).toFixed(2) + "G" }
+    else if (abs_y >= 1048576)       { return sign+(y / 1048576).toFixed(2) + "M" }
+    else if (abs_y >= 1024)          { return sign+(y / 1024).toFixed(2) + "K" }
+    else if (abs_y < 1 && abs_y > 0) { return sign+y.toFixed(2) }
+    else if (abs_y === 0)            { return sign+'0' }
+    else                             { return sign+y }
 };
